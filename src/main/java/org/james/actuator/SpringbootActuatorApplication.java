40665dfeac1e50fd2e6b27f6b5c57b6e4efff137
gongjian <gongjian@gongjian-rmbp>
2018-03-09 19:28:23 +0800

    spring boot actuator

---
+++ src/main/java/org/james/actuator/SpringbootActuatorApplication.java
@@ -0,0 +1,12 @@
+package org.james.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActuatorApplication.class, args);
+	}
+}
